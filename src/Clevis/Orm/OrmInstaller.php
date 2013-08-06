a6be49574150d32d7387ef8ba74f548ec98c0162
Vlasta Neubauer <paranoiq@centrum.cz>
2013-08-06 11:31:16 +0200

    OrmInstaller

---
+++ src/Clevis/Orm/OrmInstaller.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Clevis\Orm;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+use Composer\Repository\InstalledRepositoryInterface;
+
+
+/**
+ * Instalátor pro PetrP/Orm
+ */
+class OrmInstaller extends LibraryInstaller
+{
+
+	/**
+	 * Kontrola podpory
+	 */
+	public function supports($packageType)
+	{
+		return $packageType === 'petrp-orm';
+	}
+
+	/**
+	 * Instaluje balíček
+	 */
+	public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+	{
+		parent::install($repo, $package);
+
+		$installDir = $this->getPackageBasePath($package);
+		$this->filesystem->removeDirectory($installDir . '/Builder/libs');
+		$this->filesystem->removeDirectory($installDir . '/tests/libs');
+	}
+
+	/**
+	 * Aktualizuje balíček
+	 */
+	public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
+	{
+		parent::update($repo, $initial, $target);
+
+		$installDir = $this->getPackageBasePath($target);
+		$this->filesystem->removeDirectory($installDir . '/tests/libs');
+	}
+
+}
